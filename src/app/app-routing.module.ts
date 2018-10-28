60284c9dedcca9b81cb1b238357e6f7d91d8e637
n9exorcist <n9exorcist@gmail.com>
2018-10-28 18:19:41 +0530

    Added Routing and included css in index.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from 'src/app/login/login.component';
+import { OfferRideComponent } from 'src/app/offer-ride/offer-ride.component';
+import { RideDetailsComponent } from 'src/app/ride-details/ride-details.component';
+import { UpdateRideComponent } from 'src/app/update-ride/update-ride.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'offer-ride', component: OfferRideComponent },
+  { path: 'ride-details', component: RideDetailsComponent },
+  { path: 'update-ride/:id', component: UpdateRideComponent },
+  { path: 'update-ride', component: UpdateRideComponent },
+  { path: '', component: LoginComponent, pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
